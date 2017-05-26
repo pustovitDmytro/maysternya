d33d6f81f642134320ac83460fa9be36680a69ae
Tanya Slipachenko <nickolaychuk.t@gmail.com>
2017-05-26 17:49:08 +0100

    first

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -28,6 +28,18 @@ ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
 tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
 consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
 imperdiet lectus luctus at.</p>
+ <p>In hac habitasse platea dictumst. Duis sagittis dui ac ex suscipit maximus.
+Morbi pellentesque venenatis felis sed convallis. Nulla varius, nibh vitae
+placerat tempus, mauris sem elementum ipsum, eget sollicitudin nisl est vel
+purus. Fusce malesuada odio velit, non cursus leo fermentum id. Cras pharetra
+sodales fringilla. Etiam quis est a dolor egestas pellentesque. Maecenas non
+scelerisque purus, congue cursus arcu. Donec vel dapibus mi. Mauris maximus
+posuere placerat. Sed et libero eu nibh tristique mollis a eget lectus. Donec
+interdum augue sollicitudin vehicula hendrerit. Vivamus justo orci, molestie
+ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
+tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
+consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
+imperdiet lectus luctus at.</p>
         <div className={s.redbox}>MAP!!!</div>
       </div>
     );
