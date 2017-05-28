81f31125b028bc54d26a847c2d2b122daa4b2407
Tanya Slipachenko <nickolaychuk.t@gmail.com>
2017-05-28 21:42:30 +0100

    footer start

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,30 +11,32 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Footer.css';
 import Link from '../Link';
+import Forma from '../Forma';
 import Map from '../Map';
 
 
-class Map2 extends React.Component {
-  render() {
-    return (
-      <div class="watch me">
-        <p>In dima branch In hac habitasse platea dictumst. Duis sagittis dui ac ex suscipit maximus.
-Morbi pellentesque venenatis felis sed convallis. Nulla varius, nibh vitae
-placerat tempus, mauris sem elementum ipsum, eget sollicitudin nisl est vel
-purus. Fusce malesuada odio velit, non cursus leo fermentum id. Cras pharetra
-sodales fringilla. Etiam quis est a dolor egestas pellentesque. Maecenas non
-scelerisque purus, congue cursus arcu. Donec vel dapibus mi. Mauris maximus
-posuere placerat. Sed et libero eu nibh tristique mollis a eget lectus. Donec
-interdum augue sollicitudin vehicula hendrerit. Vivamus justo orci, molestie
-ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
-tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
-consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
-imperdiet lectus luctus at.</p>
-        <div>MAP2!!!</div>
-      </div>
-    );
-  }
-}
+
+// class Map2 extends React.Component {
+//   render() {
+//     return (
+//       <div class="watch me">
+//         <p>In dima branch In hac habitasse platea dictumst. Duis sagittis dui ac ex suscipit maximus.
+// Morbi pellentesque venenatis felis sed convallis. Nulla varius, nibh vitae
+// placerat tempus, mauris sem elementum ipsum, eget sollicitudin nisl est vel
+// purus. Fusce malesuada odio velit, non cursus leo fermentum id. Cras pharetra
+// sodales fringilla. Etiam quis est a dolor egestas pellentesque. Maecenas non
+// scelerisque purus, congue cursus arcu. Donec vel dapibus mi. Mauris maximus
+// posuere placerat. Sed et libero eu nibh tristique mollis a eget lectus. Donec
+// interdum augue sollicitudin vehicula hendrerit. Vivamus justo orci, molestie
+// ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
+// tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
+// consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
+// imperdiet lectus luctus at.</p>
+//         <div className={s.redbox}>MAP2!!!</div>
+//       </div>
+//     );
+//   }
+// }
 
 
 
@@ -42,20 +44,10 @@ class Footer extends React.Component {
   render() {
     return (
       <div className={s.root}>
+        <Forma></Forma>
         <Map></Map>
-        <Map2></Map2>
-        <ytututuut/>
-        <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/">Home</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/admin">Admin</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">Privacy</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">Not Found</Link>
-        </div>
+        
+        
       </div>
     );
   }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -11,36 +11,29 @@ import React from 'react';
 import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './map.css';
+import mapUrl from './map.jpg';
 
 class Map extends React.Component {
   render() {
     return (
-      <div class="watch me">
-        <p>In hac habitasse platea dictumst. Duis sagittis dui ac ex suscipit maximus.
-Morbi pellentesque venenatis felis sed convallis. Nulla varius, nibh vitae
-placerat tempus, mauris sem elementum ipsum, eget sollicitudin nisl est vel
-purus. Fusce malesuada odio velit, non cursus leo fermentum id. Cras pharetra
-sodales fringilla. Etiam quis est a dolor egestas pellentesque. Maecenas non
-scelerisque purus, congue cursus arcu. Donec vel dapibus mi. Mauris maximus
-posuere placerat. Sed et libero eu nibh tristique mollis a eget lectus. Donec
-interdum augue sollicitudin vehicula hendrerit. Vivamus justo orci, molestie
-ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
-tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
-consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
-imperdiet lectus luctus at.</p>
- <p>In hac habitasse platea dictumst. Duis sagittis dui ac ex suscipit maximus.
-Morbi pellentesque venenatis felis sed convallis. Nulla varius, nibh vitae
-placerat tempus, mauris sem elementum ipsum, eget sollicitudin nisl est vel
-purus. Fusce malesuada odio velit, non cursus leo fermentum id. Cras pharetra
-sodales fringilla. Etiam quis est a dolor egestas pellentesque. Maecenas non
-scelerisque purus, congue cursus arcu. Donec vel dapibus mi. Mauris maximus
-posuere placerat. Sed et libero eu nibh tristique mollis a eget lectus. Donec
-interdum augue sollicitudin vehicula hendrerit. Vivamus justo orci, molestie
-ac sollicitudin ac, lobortis at tellus. Etiam rhoncus ullamcorper risus eu
-tempor. Sed porttitor, neque ac efficitur gravida, arcu lacus pharetra dui, in
-consequat elit tellus auctor nulla. Donec placerat elementum diam, vitae
-imperdiet lectus luctus at.</p>
-        <div className={s.redbox}>MAP!!!</div>
+      <div className={s.map}>
+        <div className={s.map_img}>
+        <img src={mapUrl} width="660" height="320" alt="Map" />
+        	
+        </div>
+        <div className={s.map_contact}>
+        	<h4>Контакти</h4>
+        	<p className={s.map_tel}>+38 066 445 59 00</p>
+        	<p className={s.map_contact_adress}>м. Мукачеко вул. Переяславська, 1</p>
+   
+        		<ul className={s.map_link}>
+        			<li><a href='#'>Каталог продукції</a></li>
+        			<li><a href='#'>Про нас</a></li>
+        			<li><a href='#'>Наші роботи</a></li>
+        		</ul>
+ 
+        </div>
+        
       </div>
     );
   }
